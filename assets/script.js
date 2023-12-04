e8339eef15ce61ea29b9aa4589236f32068556c3
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-04 18:26:05 -0500

    added user interaction for form submit

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,28 @@ var savedSearches = [];
 
 // FUNCTIONS
 
+displayCurrentWeather();
+
+displayFiveDay();
+
 // USER INTERACTION
+
+// user submits form
+$("#form-location").on("submit", function (event) {
+  event.preventDefault();
+
+  // get city name
+  var cityName = $("#search-input").val();
+
+  // alert if user enters no city or unknown city
+  if (cityName === "" || cityName == null) {
+    alert("Please enter name of valid city.");
+    return;
+  } else {
+    // functions to display current and 5 day forecast
+    displayCurrentWeather();
+    displayFiveDay();
+  }
+});
 
 // INITIALIZATION
