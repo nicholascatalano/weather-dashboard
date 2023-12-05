77b6d8811a3f1eef1278fa880143e1c5c3b24871
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-05 17:49:36 -0500

    added variables to append div to current weather box, then to append the city name to div

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,8 @@ console.log(apiKey);
 // FUNCTIONS
 
 // function to display weather, both current and forecast
-searchButton.click(function displayWeather() {
+searchButton.click(function displayWeather(event) {
+  event.preventDefault();
   // variable to grab search input value
   var cityName = $(".cityName").val().trim();
   // api urls for current day and forecast
@@ -39,20 +40,28 @@ searchButton.click(function displayWeather() {
       url: apiUrlCurrent,
       method: "GET",
     }).then(function (response) {
-      // saves current city to var, then appends it to unordered list element
-      var currentCity = $(".list-group").addClass("list-group-item");
-      currentCity.append("<li>" + response.name + "</li>");
-
+      // log to confirm no response error
+      console.log(response);
       // saving name to local storage to grab later
       var local = localStorage.setItem(keyCount, response.name);
       keyCount = keyCount + 1;
 
+      // appends div to current weather, then appends p tag to the card
+      var currentWeatherCard = $(".currentCard")
+        .append("<div>")
+        .addClass("card-body");
+      currentWeatherCard.empty();
+      var currentName = currentWeatherCard.append("<p>");
+
+      currentWeatherCard.append(currentName);
+
+      // saves current city to var, then appends it to search history element
+      var searchItem = $(".list-group").addClass("list-group-item");
+      searchItem.append("<li>" + response.name + "</li>");
+
       // var to track the current date using the dt object in the response and appending it to city name
       var timeUTC = new Date(response.dt * 1000);
-      currentCity.append(" " + "(" + timeUTC.toLocaleDateString("en-US") + ")");
-      currentCity.append(
-        `<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`
-      );
+      // );
     });
   }
 });
